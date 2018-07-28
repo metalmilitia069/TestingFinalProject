67d2d463c1933b00d824ba58874247bc0c49238a
LEONARDO MEDINA <medina.engineering@outlook.com>
2018-07-28 03:20:11 -0400

    commit 1 - test

---
+++ TestingFinalProject/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TestingFinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 207);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(148, 24);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 219);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(148, 24);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(846, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestingFinalProject/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingFinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        Rectangle rect; //= new Rectangle(125, 125, 50, 50);
+
+        Rectangle mousePivot = new Rectangle(MousePosition.X, MousePosition.Y, 10, 10);
+        Point rectanglePivot = new Point(0, 0);
+
+
+
+
+        bool isMouseDown = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //rect = new Rectangle(125, 125, 50, 50);
+            //rect = new Rectangle(200, 200, 50, 50);
+        }
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.RoyalBlue), rect);
+            e.Graphics.FillRectangle(new SolidBrush(Color.RoyalBlue), mousePivot);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePivot.X = e.X;
+            mousePivot.Y = e.Y;
+
+
+            Refresh();
+            if (isMouseDown == true)
+            {
+                if (rect.IntersectsWith(mousePivot))
+                {
+                    rectanglePivot.X = e.Location.X - (rect.Width / 2);
+                    rectanglePivot.Y = e.Location.Y - (rect.Height / 2);
+
+                    rect.Location = rectanglePivot;
+
+                    if (rect.Right > pictureBox1.Width)
+                    {
+                        rect.X = pictureBox1.Width - rect.Width;
+                    }
+                    if (rect.Top < 0)
+                    {
+                        rect.Y = 0;
+                    }
+                    if (rect.Left < 0)
+                    {
+                        rect.X = 0;
+                    }
+                    if (rect.Bottom > pictureBox1.Height)
+                    {
+                        rect.Y = pictureBox1.Height - rect.Height;
+                    }
+                    Refresh();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Rectangle rect1 = new Rectangle(125, 125, 50, 50);
+            
+            rect1 = rect;
+            Point gravity = new Point(0, 0);
+            gravity = rect.Location;
+            Point stop = new Point(0, 0);
+            stop = rect.Location;
+            pictureBox3.Top = pictureBox2.Location.Y + pictureBox2.Height / 2;
+            if (rect.IntersectsWith(pictureBox2.Bounds))
+            {
+                rect.Bottom.Equals((pictureBox2.Top - (pictureBox2.Height / 2)));
+                Refresh();
+            }
+            else
+            {
+                if (rect.Bottom < pictureBox1.Height)
+                {
+                    gravity.Y++;
+                    rect.Location = gravity;
+                    Refresh();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rect = new Rectangle(200, 200, 50, 50);
+        }
+    }
+}
